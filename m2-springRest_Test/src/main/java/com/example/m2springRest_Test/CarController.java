d3d6bdc54f4f6302c1d4f410dcef4f12afe7dc0f
David Sans <dsans79@yahoo.es>
2021-10-11 12:53:34 +0200

    add GetMapping

--- m2-springRest_Test/src/main/java/com/example/m2springRest_Test/CarController.java
+++ m2-springRest_Test/src/main/java/com/example/m2springRest_Test/CarController.java
@@ -1,6 +1,7 @@
 package com.example.m2springRest_Test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,6 +15,35 @@ public Car FindCar(){
     return toyota;
 
 }
+@GetMapping("/search/{id}")
+public Car FindById(@PathVariable Long id) {
+    Car toyota = new Car(id,"Toyota","Prius");
+    return toyota;
+
+
+}
+@GetMapping("/coche/{fabricante}")
+public List<Car> FindByFabricante(@PathVariable String fabricante) {
+    Car seat = new Car(2L,fabricante,"Leon");
+    Car ford = new Car(3L,fabricante,"Puma");
+    List<Car> coche= new ArrayList<Car>(); ;
+    coche.add(seat);
+    coche.add(ford);
+    System.out.println("el fabricante a filtrar es "+fabricante);
+return coche;
+}
+@GetMapping("/coche/{fabricante}/{model}")
+public List<Car> findAllByFabricanteAndModel(@PathVariable String fabricante, @PathVariable String model) {
+    Car seat = new Car(2L,fabricante,model);
+    Car ford = new Car(3L,fabricante,model);
+    List<Car> coche= new ArrayList<Car>(); ;
+    coche.add(seat);
+    coche.add(ford);
+
+    System.out.println("el fabricante a filtrar es "+fabricante+ "el modelo " +model) ;
+    return coche;
+}
+
 @GetMapping("/lista")
 public List<Car> findAll(){
     Car seat = new Car(2L,"Seat","Leon");
